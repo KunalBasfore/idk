072718e0eb375ac439353bd5d8dd01579ba84256
KunalBasfore <144116675+KunalBasfore@users.noreply.github.com>
2023-09-07 17:30:39 +0530

    Update idk.py

--- idk.py
+++ idk.py
@@ -1,34 +1,43 @@
 import requests
 
-
-headers={
+headers = {
     "Content-Type": "application/json",
 }
-proc=f"46.101.163.117:29330" # "username:password@host:port"  (without http:// or https:// )
-num="14086750126" # example "919988776655" (without + )
-data={
-    "api-key":"gamer",
-    "number": num,
-    "proxy":proc
-}
 
-r=requests.post("http://128.140.99.16:6969/api/send-sms",json=data,headers=headers,timeout=100)
-if "securetoken" in r.text:
-    print(r.text)
-    r=r.json()
-    securetoken=r['message']['securetoken']
-    otp=str(input("otp : "))
-    data={
-    "api-key":"gamer",
-    "securetoken": securetoken,
-    "proxy":proc,
-    "otp":otp
+proc = str(input("Enter Your Proxy -> "))  # "username:password@host:port" (without http:// or https:// )
+num = str(input("Enter Your Number -> "))  # 919988998877
+
+def creator():
+    data = {
+        "api-key": "gamer",
+        "number": num,
+        "proxy": proc
     }
 
-    r=requests.post("http://128.140.99.16:6969/api/create-acc",json=data,headers=headers,timeout=100)
-    print(r.text)
+    r = requests.post("http://128.140.99.16:6969/api/send-sms", json=data, headers=headers, timeout=100)
+    if "securetoken" in r.text:
+        print(r.text)
+        r = r.json()
+        securetoken = r['message']['securetoken']
+        otp = str(input("otp : "))
+        data = {
+            "api-key": "gamer",
+            "securetoken": securetoken,
+            "proxy": proc,
+            "otp": otp
+        }
 
+        r = requests.post("http://128.140.99.16:6969/api/create-acc", json=data, headers=headers, timeout=100)
+        print(r.text)
 
+    elif "IndexError" in r.text:
+        print("indexerror restarting")
+        creator()
 
-else:
-    print(r.text)
+    elif "ReadTimeout" in r.text:
+        print("ReadTimeout restarting")
+        creator()
+    else:
+        print(r.text)
+
+creator()
